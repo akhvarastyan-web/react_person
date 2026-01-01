044a1ec03bfb174aa628368869afdbc487d64488
akhvarastyan-web <a.khvarastyan@gmail.com>
2026-01-01 19:09:32 +0200

    third

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,15 +1,16 @@
 export const Person = ({ person }) => {
   const { name, age, sex, isMarried, partnerName } = person;
-  const partnerRole = sex === 'm' ? 'wife' : 'husband';
+  const partnerRole = sex === 'm' // перевірка
+  ? 'wife' // якщо так
+  : 'husband';
 
   return (
     <section className="Person">
       <h2 className="Person__name">My name is {name}</h2>
       {age && <p className="Person__age">I am {age}</p>}
       <p className="Person__partner">
-        {/* prettier-ignore */}
-        {isMarried
-        ? `${partnerName} is my ${partnerRole}`
+        {isMarried // перевірка
+        ? `${partnerName} is my ${partnerRole}` // якщо так
         : 'I am not married'}
       </p>
     </section>
